d1e26c8b8b8d7617355486a9488095a646d5e661
anirrudh <anik597@gmail.com>
2023-06-22 13:04:25 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,30 @@
-import sys
 from setuptools import setup
 
 import rafe
 
 
 setup(
-    name = "rafe",
-    version = rafe.__version__,
-    author = "Anirrudh Krishnan, Dillon Roach, Ilan Schnell",
-    url = "https://github.com/Quansight/rafe",
-    license = "BSD",
-    description = "A build and environment analysis tool for Python",
-    long_description = open('README.md').read(),
-    packages = ['rafe'],
-    entry_points = {'console_scripts': [
-        'rafe = rafe.main:cli',
-    ]},
-    install_requires = [
-        'typer >= 0.7.0',
-        'rich',
-        'aiohttp',
-        'aiofiles',
-        'pyyaml',
-        'requests',
-        'pydantic'
+    name="rafe",
+    version=rafe.__version__,
+    author="Anirrudh Krishnan, Dillon Roach, Ilan Schnell",
+    url="https://github.com/Quansight/rafe",
+    license="BSD",
+    description="A build and environment analysis tool for Python",
+    long_description=open("README.md").read(),
+    packages=["rafe"],
+    entry_points={
+        "console_scripts": [
+            "rafe = rafe.main:cli",
+        ]
+    },
+    install_requires=[
+        "typer >= 0.7.0",
+        "rich",
+        "aiohttp",
+        "aiofiles",
+        "pyyaml",
+        "requests",
+        "pydantic",
     ]
-    #package_data = {'examples': ['*/*']},
+    # package_data = {'examples': ['*/*']},
 )
