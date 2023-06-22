5ac9302120491721ac5b293a5f2abced0926d2b7
anirrudh <anik597@gmail.com>
2023-06-22 12:53:18 -0400

    format with black

--- rafe/cli_api.py
+++ rafe/cli_api.py
@@ -11,9 +11,18 @@ from rich import print
 from rich.progress import Progress
 from rafe.cfgraph import CFGraph
 
-log_color = lambda color, hl_string, string: f'[bold {color}]' + hl_string + '[/bold' + f' {color}] | ' + str(string)
+log_color = (
+    lambda color, hl_string, string: f"[bold {color}]"
+    + hl_string
+    + "[/bold"
+    + f" {color}] | "
+    + str(string)
+)
 
-def load_repodata(cfgraph: CFGraph, task_id, progress: Progress, arch: str) -> typing.Union[str, bool]:
+
+def load_repodata(
+    cfgraph: CFGraph, task_id, progress: Progress, arch: str
+) -> typing.Union[str, bool]:
     """
     A wrapper function to load repodata objects into a collection of JSON formatted strings.
     """
@@ -26,17 +35,22 @@ def load_repodata(cfgraph: CFGraph, task_id, progress: Progress, arch: str) -> t
         return noarch_json_object
 
     if arch == "linux-64":
-        linux64_json_object = cfgraph.load_repodata(arch="linux-64") 
+        linux64_json_object = cfgraph.load_repodata(arch="linux-64")
         progress.update(task_id, advance=1)
         return linux64_json_object
 
     if arch == "win64":
-        cfgraph.logger.error("Windows 64-Bit is unsupported. Please raise an issue with the developers if you would like to see this.")
+        cfgraph.logger.error(
+            "Windows 64-Bit is unsupported. Please raise an issue with the developers if you would like to see this."
+        )
 
     if arch == "win32":
-        cfgraph.logger.error("Windows 64-Bit is unsupported. Please raise an issue with the developers if you would like to see this.")
-    
+        cfgraph.logger.error(
+            "Windows 64-Bit is unsupported. Please raise an issue with the developers if you would like to see this."
+        )
+
     return False
+
 
 def update_all_repodata(cfgraph: CFGraph, task_id, progress: Progress):
     """
@@ -45,36 +59,52 @@ def update_all_repodata(cfgraph: CFGraph, task_id, progress: Progress):
     """
     # grayskull
     if not cfgraph.check_update_repodata(cfgraph.config.grayskull_map):
-        cfgraph.logger.info(log_color('green', 'CACHED', cfgraph.config.grayskull_map), extra = {"markup": True})
+        cfgraph.logger.info(
+            log_color("green", "CACHED", cfgraph.config.grayskull_map),
+            extra={"markup": True},
+        )
         progress.update(task_id, advance=2)
     else:
-        fetch_repodata_file(cfgraph.urls.grayskull_map, cfgraph.config.grayskull_map, progress)
+        fetch_repodata_file(
+            cfgraph.urls.grayskull_map, cfgraph.config.grayskull_map, progress
+        )
         progress.update(task_id, advance=2)
 
     # linux-64
     if not cfgraph.check_update_repodata(cfgraph.config.linux64_json):
-        cfgraph.logger.info(log_color('green', 'CACHED', cfgraph.config.linux64_json), extra={"markup": True}) 
+        cfgraph.logger.info(
+            log_color("green", "CACHED", cfgraph.config.linux64_json),
+            extra={"markup": True},
+        )
         progress.update(task_id, advance=2)
     else:
-        fetch_repodata_file(cfgraph.urls.linux64_repodata, cfgraph.config.linux64_json, progress)
+        fetch_repodata_file(
+            cfgraph.urls.linux64_repodata, cfgraph.config.linux64_json, progress
+        )
         progress.update(task_id, advance=2)
 
     # noarch
     if not cfgraph.check_update_repodata(cfgraph.config.noarch_json):
-        cfgraph.logger.info(log_color('green', 'CACHED', cfgraph.config.noarch_json), extra = {"markup": True}) 
+        cfgraph.logger.info(
+            log_color("green", "CACHED", cfgraph.config.noarch_json),
+            extra={"markup": True},
+        )
         progress.update(task_id, advance=2)
     else:
-        fetch_repodata_file(cfgraph.urls.noarch_repodata, cfgraph.config.noarch_json, progress)
+        fetch_repodata_file(
+            cfgraph.urls.noarch_repodata, cfgraph.config.noarch_json, progress
+        )
         progress.update(task_id, advance=2)
+
 
 def fetch_all_package_jsons(to_fetch: typing.List[typing.Tuple], progress: Progress):
     sem = asyncio.BoundedSemaphore(10)
-    #session = aiohttp.ClientSession(trust_env=True)
+    # session = aiohttp.ClientSession(trust_env=True)
 
     async def fetch_file(url: str, destination: pathlib.Path):
         async with sem, aiohttp.ClientSession(trust_env=True) as session:
             async with session.get(url) as response:
-                print(f'Get {url}')
+                print(f"Get {url}")
                 """
                 total_size = int(response.headers.get("Content-Length", 0))
                 if total_size == 0:
@@ -84,11 +114,10 @@ def fetch_all_package_jsons(to_fetch: typing.List[typing.Tuple], progress: Progr
                 """
                 assert response.status == 200
                 data = await response.read()
-            #print(type(data), type(response))
-
+            # print(type(data), type(response))
 
         async with aiofiles.open(destination, "wb") as out:
-            print(f'Writing | {destination.name}')
+            print(f"Writing | {destination.name}")
             await out.write(data)
 
     loop = asyncio.get_event_loop()
@@ -97,10 +126,11 @@ def fetch_all_package_jsons(to_fetch: typing.List[typing.Tuple], progress: Progr
     loop.close()
     return
 
+
 def fetch_repodata_file(url: str, destination: pathlib.Path, progress: Progress):
     """
     Fetches the repodata files with special streaming with the URL. Saves it to the
-    filepath defined by destination. 
+    filepath defined by destination.
     """
     response = requests.get(url, stream=True)
     response.raise_for_status()
@@ -111,13 +141,16 @@ def fetch_repodata_file(url: str, destination: pathlib.Path, progress: Progress)
         meta = site.info()
         total_size = int(meta["Content-Length"])
 
-
-    task_id = progress.add_task(f"[cyan] Get {destination.name} ({total_size/(1024*1024):.2f}Mb):" + " [/cyan]", total=total_size)
+    task_id = progress.add_task(
+        f"[cyan] Get {destination.name} ({total_size/(1024*1024):.2f}Mb):" + " [/cyan]",
+        total=total_size,
+    )
 
     with open(destination, "wb") as file:
         for chunk in response.iter_content(chunk_size=8192):
             file.write(chunk)
             progress.update(task_id, advance=len(chunk))
+
 
 def match_nearest_version(how: str = "package") -> None:
     """
@@ -128,67 +161,111 @@ def match_nearest_version(how: str = "package") -> None:
 
     pass
 
-def missed_and_matched_from_manifest(cfgraph, package_arch, python_version_requested, manifest_packages, drop_versions, json_object):
+
+def missed_and_matched_from_manifest(
+    cfgraph,
+    package_arch,
+    python_version_requested,
+    manifest_packages,
+    drop_versions,
+    json_object,
+):
     """
     A gigantic loop to be moved outside of here, and to the cli.
     """
 
     if python_version_requested != None:
-        python_version_matches = [i for i in json_object["packages"] if python_version_requested in i]
+        python_version_matches = [
+            i for i in json_object["packages"] if python_version_requested in i
+        ]
 
     else:
         python_version_matches = json_object["packages"]
-    
-    package_report = {
-            'matched_packages' : [],
-            'missed_packages' : []
-    }
+
+    package_report = {"matched_packages": [], "missed_packages": []}
 
     converted_grayskull_map = cfgraph.convert_grayskull_map()
 
     for i in manifest_packages:
-
-        if ('pyVer' in i.keys()) and (i['pyVer'] == '2.7'):
-            cfgraph.logger.info("[bold yellow] Skipping [/bold yellow]" + f"{i['name']}=={i['version']}, pyVer = {i['pyVer']}", extra={"markup": True})
+        if ("pyVer" in i.keys()) and (i["pyVer"] == "2.7"):
+            cfgraph.logger.info(
+                "[bold yellow] Skipping [/bold yellow]"
+                + f"{i['name']}=={i['version']}, pyVer = {i['pyVer']}",
+                extra={"markup": True},
+            )
             continue
 
         i["name"] = cfgraph.convert_pypi_conda_name(i["name"], converted_grayskull_map)
-        i["name"] = i["name"].lower() #conda packages are all lower
+        i["name"] = i["name"].lower()  # conda packages are all lower
         i["version"] = i["version"].split("+")[0]
 
-        if i["version"] == "0.0.0" or len(i["version"].split("."))>3 or drop_versions:
+        if i["version"] == "0.0.0" or len(i["version"].split(".")) > 3 or drop_versions:
             i["version"] = ""
 
-        matched_package = cfgraph.match_versioned_packages(i["name"], i["version"], python_version_matches)
+        matched_package = cfgraph.match_versioned_packages(
+            i["name"], i["version"], python_version_matches
+        )
 
         if len(matched_package) > 0:
             breakpoint()
             cfgraph.fetch_package_json(i["name"], matched_package[0], package_arch)
-            package_report["matched_packages"].append({"name" : i['name'],"version" : i['version'] })
-            cfgraph.logger.info("[bold green] Match Found [/bold green]" + f"{i['name']}=={i['version']}", extra={"markup": True})
+            package_report["matched_packages"].append(
+                {"name": i["name"], "version": i["version"]}
+            )
+            cfgraph.logger.info(
+                "[bold green] Match Found [/bold green]"
+                + f"{i['name']}=={i['version']}",
+                extra={"markup": True},
+            )
 
-        elif 'eggName' in i:
-
-            i["eggName"] = cfgraph.convert_pypi_conda_name(i["eggName"], converted_grayskull_map)
+        elif "eggName" in i:
+            i["eggName"] = cfgraph.convert_pypi_conda_name(
+                i["eggName"], converted_grayskull_map
+            )
             i["eggName"] = i["eggName"].lower()
 
-            matched_package = cfgraph.match_versioned_packages(i["eggName"], i["version"], python_version_matches)
+            matched_package = cfgraph.match_versioned_packages(
+                i["eggName"], i["version"], python_version_matches
+            )
 
             if len(matched_package) > 0:
-                cfgraph.fetch_package_json(i["eggName"], matched_package[0], package_arch)
-                package_report["matched_packages"].append({"name" : i['name'],"version" : i['version'] })
-                cfgraph.logger.info("[bold green] Match Found [/bold green]" + f"{i['name']}/{i['eggName']}=={i['version']}", extra={"markup": True})
+                cfgraph.fetch_package_json(
+                    i["eggName"], matched_package[0], package_arch
+                )
+                package_report["matched_packages"].append(
+                    {"name": i["name"], "version": i["version"]}
+                )
+                cfgraph.logger.info(
+                    "[bold green] Match Found [/bold green]"
+                    + f"{i['name']}/{i['eggName']}=={i['version']}",
+                    extra={"markup": True},
+                )
 
             else:
-                package_report["missed_packages"].append({"name" : i['name'],"version" : i['version'] })
-                cfgraph.logger.info("[bold red] No Matches [/bold red]" + f"{i['name']}/{i['eggName']}=={i['version']}", extra={"markup": True})
+                package_report["missed_packages"].append(
+                    {"name": i["name"], "version": i["version"]}
+                )
+                cfgraph.logger.info(
+                    "[bold red] No Matches [/bold red]"
+                    + f"{i['name']}/{i['eggName']}=={i['version']}",
+                    extra={"markup": True},
+                )
         else:
-            package_report["missed_packages"].append({"name" : i['name'],"version" : i['version'] })
-            cfgraph.logger.info("[bold red] No Matches [/bold red]"  + f"{i['name']}=={i['version']}", extra={"markup": True})
-    
-    output = cfgraph.config.package_reports.joinpath(pathlib.Path(f'cfgraph-package-report-{datetime.now()}.json'))
+            package_report["missed_packages"].append(
+                {"name": i["name"], "version": i["version"]}
+            )
+            cfgraph.logger.info(
+                "[bold red] No Matches [/bold red]" + f"{i['name']}=={i['version']}",
+                extra={"markup": True},
+            )
+
+    output = cfgraph.config.package_reports.joinpath(
+        pathlib.Path(f"cfgraph-package-report-{datetime.now()}.json")
+    )
 
     with open(output, "w") as write_file:
-       json.dump(package_report, write_file)
-       cfgraph.logger.info(f'[bold pink] WROTE SUMMARY FILE: ' + '[/bold pink]' + str(output), extra={"markup": True})
-
+        json.dump(package_report, write_file)
+        cfgraph.logger.info(
+            f"[bold pink] WROTE SUMMARY FILE: " + "[/bold pink]" + str(output),
+            extra={"markup": True},
+        )
--- rafe/source.py
+++ rafe/source.py
@@ -10,7 +10,7 @@ from rafe.metadata import render_recipe
 
 
 def get_dir():
-    lst = [fn for fn in os.listdir(work_dir) if not fn.startswith('.')]
+    lst = [fn for fn in os.listdir(work_dir) if not fn.startswith(".")]
     if len(lst) == 1:
         dir_path = join(work_dir, lst[0])
         if isdir(dir_path):
@@ -22,15 +22,15 @@ def download_to_cache(source):
     if not isdir(src_cache):
         os.makedirs(src_cache)
 
-    fn = basename(urlparse(source['url']).path)
-    md5 = source.get('md5')
+    fn = basename(urlparse(source["url"]).path)
+    md5 = source.get("md5")
     path = join(src_cache, fn)
 
     if not isfile(path):
-        download(source['url'], path, md5)
+        download(source["url"], path, md5)
 
     assert isfile(path)
-    for ht in 'md5', 'sha1', 'sha256':
+    for ht in "md5", "sha1", "sha256":
         if ht in source and hashsum_file(path, ht) != source[ht]:
             raise Exception("%s mismatch: %r" % (ht.upper(), source))
     return path
@@ -40,18 +40,24 @@ def unpack(source):
     src_path = download_to_cache(source)
 
     os.makedirs(work_dir)
-    if src_path.endswith(('.tar.gz', '.tar.bz2', '.tgz', '.tar.xz', '.tar')):
+    if src_path.endswith((".tar.gz", ".tar.bz2", ".tgz", ".tar.xz", ".tar")):
         tar_xf(src_path, work_dir)
     else:
         raise Exception("not a vaild source")
 
 
 def apply_patch(src_dir, path):
-    print('Applying patch: %r' % path)
+    print("Applying patch: %r" % path)
     assert isfile(path), path
-    args = ['-p0', '-i', path]
-    check_call(['patch',] + args, cwd=src_dir)
-    if sys.platform == 'win32' and os.path.exists(args[-1]):
+    args = ["-p0", "-i", path]
+    check_call(
+        [
+            "patch",
+        ]
+        + args,
+        cwd=src_dir,
+    )
+    if sys.platform == "win32" and os.path.exists(args[-1]):
         os.remove(args[-1])  # clean up .patch_unix file
 
 
@@ -63,19 +69,20 @@ def provide(recipe_dir):
       - apply patches (if any)
     """
     meta = render_recipe(recipe_dir)
-    source = meta.get('source', {})
+    source = meta.get("source", {})
     rm_rf(work_dir)
-    if 'url' in source:
+    if "url" in source:
         unpack(source)
-    else: # no source
+    else:  # no source
         os.makedirs(work_dir)
 
-    if 'patch' in source:
+    if "patch" in source:
         src_dir = get_dir()
-        for patch in source.get('patches', []):
+        for patch in source.get("patches", []):
             apply_patch(src_dir, recipe_dir, patch)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from rafe.config import recipes_dir
-    provide(join(recipes_dir, 'bitarray'))
+
+    provide(join(recipes_dir, "bitarray"))
