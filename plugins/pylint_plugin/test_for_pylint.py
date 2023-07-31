d41623b74f27080fdc801ae01f783792c6c656bf
Anirrudh Krishnan <136466438+gh0stfox1337@users.noreply.github.com>
2023-07-31 17:00:00 -0400

    init pylint work

---
+++ plugins/pylint_plugin/test_for_pylint.py
@@ -0,0 +1,13 @@
+import numpy as np
+from numpy import core as CORE
+
+def test():
+    tester = np.core.numeric.restoredot()
+    return tester
+
+def test_2():
+    test()
+    #do some other things
+    another_thing = CORE.numeric.alterdot()
+    return test()
+
